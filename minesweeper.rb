9c037a82d166f1644d5e18af9d7a39058c841f43
Niranjan and Kenny <n.r.b.ramadas@gmail.com>
2013-01-15 15:12:50 -0800

    Adding basic savegame functionality, though it does not currently work

--- minesweeper.rb
+++ minesweeper.rb
@@ -20,39 +20,99 @@ class Gameboard
 		@gameboard = Array.new(board_size) {Array.new(board_size) {nil}}
 		@checked_squares = []
 		@bomb_locs = []
-
-		build_board
-		create_bombs
+		@score_board = []
 	end
 
 	def play
+		player_name = get_player_name
+		build_board
+		create_bombs
+		time = Time.now
 		while true
 			print_board
-			user_move = interface
+			user_move = get_input
 			pos = user_move[1..-1].strip.split(',').map { |el| el.to_i }
 			case user_move[0]
 			when 'r'
+				if @gameboard[pos[0]][pos[1]].bomb?
+					game_lost
+					break
+				end
 				reveal_neighbors(@gameboard[pos[0]][pos[1]])
 			when 'f'
 				flag(@gameboard[pos[0]][pos[1]])
+			when 's'
+				file = File.open("#{player_name}_savegame",'w')
+				file.write(self.to_yaml)
+				file.close
+			when 'q'
+				return
 			else
 				puts "Not a valid command, try again."
 				redo
 			end
-
+			if won
+				game_won(time, player_name)
+				break
+			end
 		end
+		puts "Play again?"
+		answer = gets.chomp.downcase
+		play if answer=='y'
+	end
+
+	private
+
+	def get_player_name
+		puts "What's your name?"
+		name = gets.chomp
+		puts "Would you like to load your previous save? (y/n)"
+		answer = gets.chomp.downcase[0]
+		if answer == 'y'
+			begin
+				o = YAML::load(File.read("#{name}_savegame"))
+				return o.play
+			rescue Errno::ENOENT
+				puts "Savegame doesn't exist, continuing without it."
+			end
+		end
+		return name
+	end
+
+	def game_lost
+		puts "Sorry, you hit a bomb!"
+		print_scoreboard
+	end
+
+	def game_won(time, player_name)
+		time = Time.now - time
+		puts "You won! Good job!"
+		puts "Game took #{time} seconds."
+		@score_board << {:player => player_name, :time => time}
+		@score_board.sort! {|a,b| b[:time] <=> a[:time]}
+		@score_board = @score_board[0...10]
+		print_scoreboard
+	end
+
+	def print_scoreboard
+		puts "\nScoreboard:"
+		puts "-----------"
+		@score_board.each do |score|
+			puts "#{score[:player]} : #{score[:time]} seconds."
+		end
+		puts
 	end
 
 	def flag(square)
 		square.display_token = 'F'
 	end
 
-	def won?
-		(@checked_squares+@bomb_locs).size == board_size**2
+	def won
+		(@checked_squares+@bomb_locs).size == @board_size**2
 	end
 
-	def interface
-		puts "[F to flag, R to reveal] (row, column)"
+	def get_input
+		puts "[F to flag, R to reveal, S to save, Q to quit] (row, column)"
 		puts "Choose a move (ex. r 0,2):"
 		print "> "
 		gets.chomp.downcase
@@ -71,7 +131,6 @@ class Gameboard
 				set_neighbors(square, y, x)
 			end
 		end
-		puts "done"
 	end
 
 	def create_bombs
@@ -79,7 +138,7 @@ class Gameboard
 			square = @gameboard.sample.sample
 			redo if @bomb_locs.include?(square)
 			square.bomb = true
-			square.display_token = "B"
+			square.display_token = "B" # used for testing
 			@bomb_locs << square
 		end
 	end
@@ -96,11 +155,32 @@ class Gameboard
 	end
 
 	def print_board
-		puts "  #{(0...@board_size).to_a.join(' ')}"
+		print "  "
+		(0...@board_size).each do |column_label|
+			if @board_size > 10
+				spaces = column_label < 10 ? '  ' : ' '
+			else
+				spaces = ' '
+			end
+			print "#{spaces}#{column_label}"
+		end
+		puts
+		print "  "
+		(0...@board_size).each do |column_label|
+			if @board_size > 10
+				spaces = column_label < 10 ? '  ' : ' '
+			else
+				spaces = ' '
+			end
+			print "#{spaces}-"
+		end
+		puts
 		(0...@board_size).each do |y|
-			print "#{y} "
+			print " " if y < 10 && @board_size > 10
+			print "#{y}| "
 			(0...@board_size).each do |x|
-				print "#{@gameboard[y][x].display_token} "
+				spaces = @board_size > 10 ? '  ' : ' '
+				print "#{@gameboard[y][x].display_token}#{spaces}"
 			end
 			puts
 		end
