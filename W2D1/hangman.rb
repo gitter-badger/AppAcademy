fc3b970607b297a6da88a60dfd8266b0ee265465
Christopher Drellow <christopherlives@gmail.com>
2013-01-14 18:07:07 -0800

    wordchains works and it is better.

--- W2D1/hangman.rb
+++ W2D1/hangman.rb
@@ -2,7 +2,70 @@ class Hangman
 
   def initialize
     @dictionary = read_dictionary
-    @winning_word = ""
+  end
+
+  def read_dictionary(filename="dictionary.txt")
+    dictionary = []
+    File.foreach(filename) do |word|
+      dictionary << word.chomp
+    end
+    dictionary
+  end
+
+  def create_players
+    puts "Who will be doing the guessing, human or robot?"
+    gets.chomp == "human" ? @player1 = HumanPlayer.new : @player1 = ComputerPlayer.new
+    puts "Who will be making up the word, human or robot?"
+    gets.chomp == "human" ? @player2 = HumanPlayer.new : @player2 = ComputerPlayer.new
+  end
+
+  def run
+    create_players
+    @winning_word = @player2.choose_word
+    while true
+
+      board = @player2.show_board(@winning_word)
+      puts board
+      players_move = @player1.move(board)
+      puts players_move
+      @player2.checkmove(players_move)
+
+      if @player2.game_won?
+        puts "You win! Word as #{@winning_word}"
+        break
+      elsif @player2.game_lost?
+        puts "You Lost!"
+        break
+      end
+
+    end
+    puts "Play again?"
+    if gets.chomp == "Y"
+      run
+    end
+  end
+
+
+end
+
+
+
+class HumanPlayer
+
+  def show_board
+  end
+
+  def choose_word
+    puts "What will the word be?"
+    word = gets.chomp
+  end
+
+end
+
+class ComputerPlayer
+
+  def initialize
+    @dictionary = read_dictionary
   end
 
   def read_dictionary(filename="dictionary.txt")
@@ -17,26 +80,4 @@ class Hangman
     @dictionary.sample
   end
 
-  def run
-    @winning_word = choose_word
-    create_players
-    while true
-      input = @player1.move
-      if @player2.game_won?
-        puts "You win! Word as #{@winning_word}"
-      elsif @player2.game_lost?
-        puts "You Lost!"
-      end
-    end
-  end
-
-
-end
-
-
-
-class HumanPlayer
-end
-
-class ComputerPlayer
 end
---
+++ W2D1/wordchains.rb
@@ -0,0 +1,70 @@
+class Wordchain
+
+  def initialize
+    build_dictionary
+  end
+
+  def adjacent_words(word, candidates)
+    adjacent_words = []
+    word.length.times do |index|
+      ("a".."z").each do |letter|
+        new_word = word.dup
+        new_word[index] = letter
+        adjacent_words << new_word if candidates.include?(new_word)
+      end
+    end
+    adjacent_words
+  end
+
+  def find_target(word, target)
+    raise if target.length != word.length
+    @candidates = @dictionary.select {|dict_word| word.length == dict_word.length}
+    @full_tree = {word => nil}
+
+    until @full_tree.has_key?(target)
+      find_next_level(@full_tree)
+    end
+
+    build_trail(word, target)
+  end
+
+  def build_trail(word, target)
+    target_value = ""
+    trail = [target]
+
+    until target_value == word
+      target_value = @full_tree[target]
+      trail << target_value
+      target = target_value
+    end
+    trail.reverse.join(" => ")
+  end
+
+
+  def find_next_level(set) 
+    return_hash = {}
+    set.each do |word, value|
+      new_array = adjacent_words(word, @candidates)
+      new_array.each do |new_word|
+        unless return_hash.has_key?(new_word)
+          return_hash.merge!({new_word => word})
+          @candidates -= [new_word]
+        end
+      end
+    end
+    if return_hash.empty?
+      raise "The goggles do nothing."
+    end
+    puts return_hash
+    @full_tree.merge!(return_hash)
+  end
+
+
+  def build_dictionary(filename="dictionary.txt")
+    dictionary = []
+    File.foreach(filename) do |word|
+      dictionary << word.chomp
+    end
+    @dictionary = dictionary
+  end
+end
